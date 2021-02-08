ebe0f2dc6f12007d3bfbf7ad3d46b295e44cf45d
Elizaveta <kashirskayaelizaveta123@gmail.com>
2021-02-08 23:15:38 +0300

    Add Widgets and Stories

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,84 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(Destini());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
+class Destini extends StatelessWidget {
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: StoryPage(),
+    );
   }
 }
+
+class StoryPage extends StatefulWidget {
+  _StoryPageState createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("images/background.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+          constraints: BoxConstraints.expand(),
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Expanded(
+                  flex: 12,
+                  child: Center(
+                    child: Text(
+                     '',
+                      style: TextStyle(
+                        fontSize: 25.0,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: FlatButton(
+                    onPressed: () {
+                    },
+                    color: Colors.red,
+                    child: Text(
+                      'Story 1',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Expanded(
+                  flex: 2,
+                  child: FlatButton(
+                    onPressed: () {
+                    },
+                    color: Colors.blue,
+                    child: Text(
+                      'Story 2',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
+
